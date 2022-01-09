e50f3fca6d2b7119b7379b1bf29228305b04727b
Mohamed ELHIRECH <mohamed.elhirech@prestataire.sacd.fr>
2022-01-09 16:13:29 +0100

    add serverless config

---
+++ lambda.js
@@ -0,0 +1,6 @@
+const awsServerlessExpress = require("aws-serverless-express");
+const app = require("./server.js");
+const server = awsServerlessExpress.createServer(app);
+
+module.exports.handler = (event, context) =>
+  awsServerlessExpress.proxy(server, event, context);
